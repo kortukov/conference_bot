93776ad0e09890177c833a3fdddcb7b29317bf2d
kortukov <elephunker1@gmail.com>
2019-02-16 20:08:22 +0300

    minor fixes

--- src/classes.py
+++ src/classes.py
@@ -1,5 +1,39 @@
 from datetime import datetime
 
+def get_place(place_id, eng = False):
+    if place_id < 0 or place_id > 11: return " "
+    places = {
+    0 : "Сокольники",
+    1 : "Пассаж",
+    2 : "Ресторан 'Москва'",
+    3 : "Охотный ряд",
+    4 : "Крымский вал",
+    5 : "Воробьёвы горы",
+    6 : "Арбат",
+    7 : "Красные ворота",
+    8 : "Остоженка",
+    9 : "Чистые пруды",
+    10: "Полянка",
+    11: "Маросейка"
+    } 
+    places_eng = {
+        0 : "Sokolniki",
+        1 : "Passage",
+        2 : "Restaurant 'Moskva'",
+        3 : "Okhotny Ryad",
+        4 : "Krymsky Val",
+        5 : "Vorobiovy Gory", 
+        6 : "Arbat",
+        7 : "Krasnye Vorota",
+        8 : "Ostozhenka",
+        9 : "Chistye Prudy",
+        10: "Polyanka",
+        11: "Maroseika"
+    }
+    if eng:
+        return places_eng[place_id]
+    else:
+        return places[place_id]
 
 class BaseEvent(object):
 
@@ -11,7 +45,7 @@ class BaseEvent(object):
         self.title_ru = title_ru
         self.title_en = title_en
     def __str__(self):
-        return "Start: " + str(datetime.fromtimestamp(self.ts_begin).time())[:-3] + "    End: " + str(datetime.fromtimestamp(self.ts_end).time())[:-3] + "\n" + str(self.place_id) + " " + str(self.title_ru) + " " + str(self.title_en)
+        return "Start: " + str(datetime.fromtimestamp(self.ts_begin).time())[:-3] + "    End: " + str(datetime.fromtimestamp(self.ts_end).time())[:-3] + "\n" + get_place(self.place_id) + "\n" + str(self.title_ru) + " " + str(self.title_en)
 
     def getEventType(self):
         return self.event_type
